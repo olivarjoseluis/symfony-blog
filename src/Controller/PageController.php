a992ae1c304c63d273a1c000de78f423225454a5
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-02-14 23:12:21 -0500

    Adding form comments

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,11 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Post;
+
 use App\Repository\PostRepository;
+
+use App\Form\CommentType;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -23,7 +30,28 @@ class PageController extends AbstractController
     #[Route('/blog/{slug}', name: 'app_post')]
     public function post(Post $post): Response
     {
-        //dd($post);
-        return $this->render('page/post.html.twig', ['post' => $post]);
+        $form = $this->createForm(CommentType::class);
+
+        return $this->render('page/post.html.twig', ['form' => $form->createView(), 'post' => $post]);
+    }
+
+    #[Route('/new-comment/{slug}', name: 'app_comment_new')]
+    public function comment(Request $request, Post $post, EntityManagerInterface $entityManager): Response
+    {
+        $comment = new Comment();
+        $comment->setUser($this->getUser());
+        $comment->setPost($post);
+
+        $form = $this->createForm(CommentType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post',['slug'=>$post->getSlug()]);
+        }
+
+        return $this->render('page/post.html.twig', ['form' => $form->createView(), 'post' => $post]);
     }
 }
